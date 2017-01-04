23f9a025d35309b064660714be50b4a8f0148690
MURFYEXP <1454342484@qq.com>
2017-01-04 10:59:16 -0800

    update

---
+++ inc/elf.h
@@ -0,0 +1,66 @@
+#ifndef JOS_INC_ELF_H
+#define JOS_INC_ELF_H
+
+#define ELF_MAGIC 0x464C457FU	/* "\x7FELF" in little endian */
+
+struct Elf {
+	uint32_t e_magic;	// must equal ELF_MAGIC
+	uint8_t e_elf[12];
+	uint16_t e_type;
+	uint16_t e_machine;
+	uint32_t e_version;
+	uint32_t e_entry;
+	uint32_t e_phoff;  //Program Header Table距离表头的偏移量
+	uint32_t e_shoff;
+	uint32_t e_flags;
+	uint16_t e_ehsize;
+	uint16_t e_phentsize;
+	uint16_t e_phnum;  //Program Header Table表中表项的个数，即段的个数
+	uint16_t e_shentsize;
+	uint16_t e_shnum;
+	uint16_t e_shstrndx;
+};
+
+//memsz一定大于等于filesz，因为段在文件中时许多未定义的变量并没有分配空间给它们
+struct Proghdr {
+	uint32_t p_type;
+	uint32_t p_offset;//相对于elf文件的开头的偏移量
+	uint32_t p_va;
+	uint32_t p_pa;    //段在内存中的物理地址
+	uint32_t p_filesz;//段在elf文件中的大小
+	uint32_t p_memsz; //段被实际装入内存后的大小
+    uint32_t p_flags;
+	uint32_t p_align;
+};
+
+struct Secthdr {
+	uint32_t sh_name;
+	uint32_t sh_type;
+	uint32_t sh_flags;
+	uint32_t sh_addr;
+	uint32_t sh_offset;
+	uint32_t sh_size;
+	uint32_t sh_link;
+	uint32_t sh_info;
+	uint32_t sh_addralign;
+	uint32_t sh_entsize;
+};
+
+// Values for Proghdr::p_type
+#define ELF_PROG_LOAD		1
+
+// Flag bits for Proghdr::p_flags
+#define ELF_PROG_FLAG_EXEC	1
+#define ELF_PROG_FLAG_WRITE	2
+#define ELF_PROG_FLAG_READ	4
+
+// Values for Secthdr::sh_type
+#define ELF_SHT_NULL		0
+#define ELF_SHT_PROGBITS	1
+#define ELF_SHT_SYMTAB		2
+#define ELF_SHT_STRTAB		3
+
+// Values for Secthdr::sh_name
+#define ELF_SHN_UNDEF		0
+
+#endif /* !JOS_INC_ELF_H */
---
+++ kern/printf.c
@@ -0,0 +1,39 @@
+// Simple implementation of cprintf console output for the kernel,
+// based on printfmt() and the kernel console's cputchar().
+
+#include <inc/types.h>
+#include <inc/stdio.h>
+#include <inc/stdarg.h>
+
+
+//把值存放到这个地址后，地址数自动增加1
+//int参数代表一个要输出的字符的值,void* 代表要把这个字符输出的位置的地址,
+static void
+putch(int ch, int *cnt)
+{
+	cputchar(ch);
+	*cnt++;
+}
+
+int
+vcprintf(const char *fmt, va_list ap)
+{
+	int cnt = 0;
+
+	vprintfmt((void*)putch, &cnt, fmt, ap);
+	return cnt;
+}
+
+int
+cprintf(const char *fmt, ...)
+{
+	va_list ap;
+	int cnt;
+
+	va_start(ap, fmt);
+	cnt = vcprintf(fmt, ap);
+	va_end(ap);
+
+	return cnt;
+}
+
